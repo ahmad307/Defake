317865f84a175d90655599f53e34517dfec39cf6
Ahmad <ahmad.hussain.307@gmail.com>
2019-10-30 02:33:29 +0200

    Updated settings.py

--- Defake/settings.py
+++ Defake/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
 STATICFILES_DIRS = [
     STATIC_DIR,
